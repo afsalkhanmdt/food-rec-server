a220eb617df726ad158f601d41fe990cad868c58
afsalkhanmdt <83784510+afsalkhanmdt@users.noreply.github.com>
2021-10-18 19:22:36 +0530

    Update otpMobile.js

--- otpMobile.js
+++ otpMobile.js
@@ -6,7 +6,7 @@ const sendOtp = async(mobileNumber) =>{
     const otp = crypto.randomInt(1000,9999);
     const req = unirest("POST", "https://www.fast2sms.com/dev/bulkV2");
     req.headers({
-    "authorization": "MqzPwSXOInQoacjrvd5fK6kFubygW4ih9UNY8A1BJZL3GxHtsDbOFU2QqJY0TmSsijwolMIe94fv7K1H"
+    "authorization": ""
     });
     req.form({
     "variables_values": otp+"",
